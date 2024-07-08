a35b502143f37730d8d5aea409b9bfcb10621909
DinoCROTH <58315706+DinoCROTH@users.noreply.github.com>
2024-07-08 09:10:17 +0200

    Add files via upload

---
+++ mt-stealfuel/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+-- Time in milliseconds for the progress bar to finish
+Config.ProgressBarTime = 5000
+
+-- Name of the fuel script to use (e.g., "ps-fuel")
+Config.FuelScript = "ps-fuel"
