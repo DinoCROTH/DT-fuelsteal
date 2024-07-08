166e287f1cf4035daaf8f6b3165190ad3ae1e360
DinoCROTH <58315706+DinoCROTH@users.noreply.github.com>
2024-07-08 09:11:18 +0200

    Delete mt-stealfuel directory

--- mt-stealfuel/config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
--- Time in milliseconds for the progress bar to finish
-Config.ProgressBarTime = 5000
-
--- Name of the fuel script to use (e.g., "ps-fuel")
-Config.FuelScript = "ps-fuel"
