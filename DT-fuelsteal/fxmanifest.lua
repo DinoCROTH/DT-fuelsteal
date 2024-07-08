bc1a36b290aea14b2ff4dad1767024cda9ddd71c
DinoCROTH <58315706+DinoCROTH@users.noreply.github.com>
2024-07-08 09:11:50 +0200

    Add files via upload

---
+++ DT-fuelsteal/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'DinoCROTH'
+
+description 'DT-fuelsteal'
+
+version '1.2'
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
